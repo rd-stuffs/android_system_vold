c5b9cab82a76393c28b294b53033efa8a84bb48f
codeworkx <codeworkx@cyanogenmod.org>
2013-01-31 14:44:16 +0000

    vold: use blkid to detect filesystem type
    In preparation for handling ext4 partitions via vold and fuse.
    
    Change-Id: Ibaccded63a7a293ad53f16afb80448ddd493554c

--- Android.mk
+++ Android.mk
@@ -29,7 +29,8 @@ common_c_includes := \
 	frameworks/native/include \
 	system/security/keystore \
 	hardware/libhardware/include/hardware \
-	system/security/softkeymaster/include/keymaster
+	system/security/softkeymaster/include/keymaster \
+	external/e2fsprogs/lib
 
 common_shared_libraries := \
 	libsysutils \
@@ -46,7 +47,8 @@ common_shared_libraries := \
 	libselinux \
 	libutils \
 	libhardware \
-	libsoftkeymaster
+	libsoftkeymaster \
+	libext2_blkid
 
 common_static_libraries := \
 	libfs_mgr \
@@ -102,7 +104,7 @@ LOCAL_MODULE:= vdc
 
 LOCAL_C_INCLUDES :=
 
-LOCAL_CFLAGS := 
+LOCAL_CFLAGS :=
 
 LOCAL_SHARED_LIBRARIES := libcutils
 
